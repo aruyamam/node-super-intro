f18ac93e774755eb764cdeeadca06442b4552fa0
aruyamam <aruyamam@gmail.com>
2018-05-13 12:18:26 +0900

    Section 3-2 アプリケーション変数

--- node-app/app.js
+++ node-app/app.js
@@ -42,21 +42,38 @@ function getFromClient(request, response) {
 }
 
 // 追加するデータ用変数
-var data = {
-    'Taro': '09-999-999',
-    'Hanako': '080-888-888',
-    'Sachiko': '070-777-777',
-    'Ichiro': '060-666-666'
-};
+var data = { msg: 'no message...' };
 
 // indexのアクセス処理
 function response_index(request, response) {
-    var msg = 'これはIndexページです。';
+    // POSTアクセス時の処理
+    if (request.method == 'POST') {
+        var body = '';
+
+        // データ受信のイベント処理
+        request.on('data', (data) => {
+            body += data;
+        });
+
+        // データ受信終了のイベント処理
+        request.on('end', () => {
+            data = qs.parse(body);
+            console.log(data);
+            write_index(request, response);
+        });
+    }
+    else {
+        write_index(request, response);
+    }
+}
+
+// indexの表示の作成
+function write_index(request, response) {
+    var msg = '※伝言を表示します。';
     var content = ejs.render(index_page, {
         title: 'Index',
         content: msg,
         data: data,
-        filename: 'data_item',
     });
 
     response.writeHead(200, { 'Content-Type': 'text/html' });
