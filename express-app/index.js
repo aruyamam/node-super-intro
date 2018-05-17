65fc219d935a2ddd5d43113899012da4b6e10daf
aruyamam <aruyamam@gmail.com>
2018-05-17 17:41:28 +0900

    Chapter 4 別のページを追加する

--- express-app/index.js
+++ express-app/index.js
@@ -7,16 +7,37 @@ app.engine('ejs', ejs.renderFile);
 
 app.use(express.static('public'));
 
+
+// ※トップページ
 app.get('/', (req, res) => {
-  const msg = 'This is Express Page!<br>'
-        + 'これは、スタイルシートを利用した例です。';
+  const msg = 'This is Index Page!<br>'
+        + 'これは、トップページです。';
 
   res.render('index.ejs', {
     title: 'Index',
-    content: msg
+    content: msg,
+    link: {
+      href: '/other',
+      text: '※別のページに移動'
+    }
+  });
+});
+
+// ※otherページ
+app.get('/other', (req, res) => {
+  const msg = 'This is Other Page!<br>'
+        + 'これは、用意された別のページです。';
+
+  res.render('index.ejs', {
+    title: 'other',
+    content: msg,
+    link: {
+      href: '/',
+      text: '※トップに戻る',
+    }
   });
 });
 
 const server = app.listen(3000, () => {
-  console.log('Server is running');
+  console.log('Server is running!');
 });
