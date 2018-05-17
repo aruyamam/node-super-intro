f5fc59603a199fd99f7f70ce11b19e6bc0126cf4
aruyamam <aruyamam@gmail.com>
2018-05-17 18:30:08 +0900

    Chapter 4 パーシャルによるテーブル表示

--- express-app/index.js
+++ express-app/index.js
@@ -8,14 +8,22 @@ app.engine('ejs', ejs.renderFile);
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+const data = {
+  'Taro'   : 'taro@yamada',
+  'Hanako' : 'hanako@flower',
+  'Sachiko': 'sachico@happy',
+  'Ichiro' : 'ichiro@baseball',
+};
+
 // ※トップページ
 app.get('/', (req, res) => {
   const msg = 'This is Index Page!<br>'
         + '※メッセージを書いて送信してください。';
 
   res.render('index.ejs', {
-    title: 'Index',
+    title  : 'Index',
     content: msg,
+    data   : data,
   });
 });
 
@@ -24,7 +32,7 @@ app.post('/', (req, res) => {
   const msg = `This is Posted Page!<br>あなたは「<b>${req.body.message}</b>」と送信しました。`
 
   res.render('index.ejs', {
-    title: 'Posted',
+    title  : 'Posted',
     content: msg,
   });
 });
