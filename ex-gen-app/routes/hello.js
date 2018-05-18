9089d271dea7eb332c703680a75a294f01d3334b
aruyamam <aruyamam@gmail.com>
2018-05-18 11:57:11 +0900

    Chapter 5 フォームの送信について

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -2,11 +2,19 @@ const express = require('express'),
       router  = express.Router();
 
 router.get('/', (req, res, next) => {
-  const name = req.query.name,
-        mail = req.query.mail,
+  const data = {
+    title: 'Hello!',
+    content: '※何か書いて送信して下さい。',
+  };
+
+  res.render('hello', data);
+});
+
+router.post('/post', (req, res, next) => {
+  const msg = req.body['message'],
         data = {
           title: 'Hello!',
-          content: `あなたの名前は、${name}。<br>メールアドレスは、${mail}です。`,
+          content: `あなたは、「${msg}」と送信しました。`,
         };
 
   res.render('hello', data);
