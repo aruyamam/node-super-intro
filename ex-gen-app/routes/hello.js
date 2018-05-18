0b62ff07ee0fc754083762d8a23c69bcf9842041
aruyamam <aruyamam@gmail.com>
2018-05-18 11:47:47 +0900

    Chapter 5 パラメーターを使おう

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -2,10 +2,12 @@ const express = require('express'),
       router  = express.Router();
 
 router.get('/', (req, res, next) => {
-  const data = {
-    title: 'Hello!',
-    content: 'これは、サンプルのコンテンツです。<br>this is sample content.',
-  };
+  const name = req.query.name,
+        mail = req.query.mail,
+        data = {
+          title: 'Hello!',
+          content: `あなたの名前は、${name}。<br>メールアドレスは、${mail}です。`,
+        };
 
   res.render('hello', data);
 });
