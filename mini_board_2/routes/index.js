c408a84d3f6a52b667fe5e27d175a0e83a6995d6
aruyamam <aruyamam@gmail.com>
2018-06-20 18:14:06 +0900

    Chapter 7 app.jsの修正とrouters/index.jsの作成

--- mini_board_2/routes/index.js
+++ mini_board_2/routes/index.js
@@ -1,9 +1,73 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var knex = require('knex')({
+  dialect: 'sqlite3',
+  connection: {
+    filename: 'board_data.sqlite3',
+  },
+  useNullAsDefault: true,
+});
+
+var Bookshelf = require('bookshelf')(knex);
+
+Bookshelf.plugin('pagination');
+
+var User = Bookshelf.Model.extend({
+  tableName: 'users',
+});
+
+var Message = Bookshelf.Model.extend({
+  tableName: 'messages',
+  hasTimestamps: true,
+  user: function () {
+    return this.belongsTo(User);
+  },
+});
+
+router.get('/:page', (req, res, next) => {
+  if (req.session.login === null) {
+    res.redirect('/users');
+    return;
+  }
+  var pg = req.params.page;
+  pg *= 1;
+  if (pg < 1) { pg = 1; }
+  new Message().orderBy('created_at', 'DESC')
+    .fetchPage({
+      page: pg,
+      pageSize: 10,
+      withRelated: ['user'],
+    })
+    .then((collection) => {
+      const data = {
+        title: 'miniBoard',
+        login: req.session.login,
+        collection: collection.toArray(),
+        pagination: collection.pagination,
+      };
+      res.render('index', data);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: true,
+        data: {
+          message: err.message,
+        }
+      });
+    });
+});
+
+router.post('/', (req, res, next) => {
+  const rec = {
+    message: req.body.msg,
+    user_id: req.session.login.id,
+  };
+  new Message(rec)
+    .save()
+    .then((model) => {
+      res.redirect('/');
+    });
 });
 
 module.exports = router;
