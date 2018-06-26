cabeade839b795f85fd907edb487a1b55292eb67
aruyamam <aruyamam@gmail.com>
2018-06-26 20:44:22 +0900

    Chapter 7-2 app.jsの作成

--- mark_app/app.js
+++ mark_app/app.js
@@ -1,11 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
+var createError  = require('http-errors');
+var express      = require('express');
+var path         = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var logger       = require('morgan');
+var session      = require('express-session');
+var validator    = require('express-validator');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var index = require('./routes/index');
+var login = require('./routes/login');
+var add   = require('./routes/add');
+var mark  = require('./routes/mark');
 
 var app = express();
 
@@ -18,9 +22,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(validator());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const session_opt = {
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 60 * 60 * 1000 },
+};
+app.use(session(session_opt));
+
+app.use('/login', login);
+app.use('/add', add);
+app.use('/mark', mark);
+app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
